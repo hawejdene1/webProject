02e93d3c769cc980aa23cc1f38b43c8a5618dac0
hawejdene1 <hawejdene@gmail.com>
2018-04-17 09:59:39 +0100

    calcul distance prix operation

---
+++ StationManagement/CalculManagementAPI.php
@@ -0,0 +1,72 @@
+<?php
+require_once "C:\Users\ASUS\Desktop\git\gestion_autoroute\StationManagement\DBinteraction\Operations.php";
+require_once "C:\Users\ASUS\Desktop\git\gestion_autoroute\StationManagement\src\Utility.php";
+require_once "C:\Users\ASUS\Desktop\git\gestion_autoroute\StationManagement\DBinteraction\CalculOperation.php";
+/**
+ * This file is what you will use for calcul prix and ditance
+
+ */
+/**
+ * @param $line
+ * @param $name1
+ * @param $name2
+ * @param $cat
+ * @return prix total , distance total , stations parcouru
+ */
+
+function caluculPrixDistance($line,$nameStart,$nameEnd,$cat){
+
+
+    if ( ! (($stationStart= getStationDB($nameStart,$line)) && ($stationEnd= getStationDB($nameEnd,$line)))) return "stations do not exist";
+    //check direction
+
+    if ($stationStart->getDist()< $stationEnd->getDist()) $keyword='ASC' ;
+    else  $keyword='DESC';
+
+
+    $array =calculPrixDistance($stationStart,$stationEnd,$keyword,$cat);
+    return $array;
+}
+
+
+
+
+
+function caluculPrix($line,$nameStart,$nameEnd,$cat){
+
+
+    if ( ! (($stationStart= getStationDB($nameStart,$line)) && ($stationEnd= getStationDB($nameEnd,$line)))) return "stations do not exist";
+    //check direction
+
+    if ($stationStart->getDist()< $stationEnd->getDist()) $keyword='ASC' ;
+    else  $keyword='DESC';
+
+
+    $array =calculPrix($stationStart,$stationEnd,$keyword,$cat);
+    return $array;
+}
+
+
+
+
+
+
+
+
+
+
+
+function stationBetween($line,$name1,$name2){
+
+    if ( ! (($stationStart= getStationDB($name1,$line)) && ($stationEnd= getStationDB($name2,$line)))) return "stations do not exist";
+
+    //check direction
+    if ($stationStart->getDist()>$stationEnd->getDist()) $keyword='DESC' ;
+    else  $keyword='ASC';
+
+    $stations=stationBetweenBD($stationStart,$stationEnd,$keyword);
+
+    return $stations;
+
+
+}
