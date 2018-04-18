86d1ee2fcdcfb0fbe21967aa999a9ff5877edb6b
Salma Rais <salmamrais@gmail.com>
2018-04-19 00:08:40 +0100

    This is the initial commit that includes the interfaces

---
+++ interface/agentDashboard.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Agent Dashboard</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="../res/css/styleDashboard.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+
+
+    <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">User Profile</a></li>
+        <li><a href="#">Disconnect</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+    <div class="container-fluid">
+      <div class="row">
+      <div class="col-sm-3 col-lg-2">
+        <nav class="navbar navbar-default navbar-fixed-side">
+             <ul class="list-group">
+              <li class="list-group-item list-group-item-info">Agent Infos</li>
+              <li class="list-group-item"> Agent ID       : </li>
+              <li class="list-group-item"> Name           : </li>
+              <li class="list-group-item"> Lastname       : </li>
+              <li class="list-group-item"> Last connection: </li>
+              <a class="list-group-item list-group-item-warning" href="#">Account configuration</a>
+             </ul>
+        </nav>
+      </div>
+      <div class="col-sm-9 col-lg-10 ">
+      <!-- your page content -->
+        <h2>Welcome to the Highway Station</h2>
+        <h1><!-- getNomPeage()--> Sousse </h1>
+
+        <p>Choose one of the functionnalities</p>
+        <div class="btn-group btn-group-justified agentButtons" role="group" aria-label="...">
+        <div class="btn-group" role="group">
+          <button type="button" class="btn btn-default">Add Ticket</button>
+        </div>
+        <div class="btn-group" role="group">
+          <button type="button" class="btn btn-default">Pay Ticket</button>
+        </div>
+        </div>
+      </div>
+      </div>
+    </div>
+      
+     
+
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="../res/js/bootstrap.min.js"></script> 
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+  </body>
+</html>
---
+++ interface/login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login Agent</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="../res/css/styleForm.css">
+  </head>
+  <body>
+
+
+    <form action="" method="post">
+    <div class="form-group">
+    <div class="row">
+      <label class="col-md-4" id="inputAgentLogin">Login</label>
+      <input class="col-md-8 form-control" type="text" name="agentLogin" placeholder="Agent Login" id="inputAgentLogin" required>
+    </div>
+
+    <div class="row">
+      <label class="col-md-4" id="inputAgentPwd">Mot de passe</label>
+      <input class="col-md-8 form-control" type="password" name="agentPwd" placeholder="Agent Password" id="inputAgentPwd" required>
+    </div>
+    </div>
+    <div class="form-group">
+      <div class="row">
+        <label class="col-md-4">Privilèges</label>
+        <div class="form-check col-md-4">
+          <input class="form-check-input" type="radio" name="radioPriviledges" id="radioAgent" checked>
+          <label class="form-check-label" for="radioAgent"> Agent</label>
+        </div>
+        <div class="form-check col-md-4">
+          <input class="form-check-input" type="radio" name="radioPriviledges" id="radioAdmin">
+          <label class="form-check-label" for="radioAdmin"> Admin</label>
+        </div>
+      </div>
+
+    </div>
+
+
+    <?php
+        echo '
+        <div class="alert alert-danger" role="alert">
+                  Alert Messages;
+        </div>';
+    ?>
+
+
+
+    </form>
+
+
+
+
+
+  </body>
+</html>
