f9451dcae15d07199e865261bf2ca7ad6bc26e7a
InafKhorchani <insafkh1997@gmail.com>
2018-04-20 20:09:49 +0100

    ajout de formulaire d'entrée

---
+++ TicketManagement/Formulaire.php
@@ -0,0 +1,44 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" >
+    <title>Title</title>
+    <style>
+
+#monForm{
+position:absolute;
+            top:50%;
+            left:50%;
+            width:400px;
+            height:400px;
+            margin-left:-150px;
+            margin-top:-100px;
+            backface-visibility:visible;
+
+        }
+
+
+
+    </style>
+</head>
+
+
+
+
+
+<body >
+
+<form  method="post" action="Entry.php" id="monForm"> <br>
+
+choisir la catégorie : <br>
+    <input type="radio" name="categorie" value="categ1" required>categ1 <br>
+    <input type="radio" name="categorie" value="categ2" required>categ2<br>
+    <input type="radio" name="categorie" value="categ3" required>categ3<br>
+    <button type="submit" value="submit" href="Imprimer.php">submit</button>
+
+</form>
+
+</body>
+</html>
