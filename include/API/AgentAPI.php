ed903cd0b68f866f09efd102ee58539dda582018
Chaker Iyadh <chaker.iyadh@gmail.com>
2018-04-27 17:26:15 +0100

    Removed station_id variable and replaced with (line,station)

--- include/API/AgentAPI.php
+++ include/API/AgentAPI.php
@@ -2,25 +2,25 @@
 
 //getAgentID getAgentLocation 
 
-
+ 
 
 require_once dirname(dirname(dirname(__FILE__))) . "/src/DBConnection.php" ;
 
 function getAgent($cin,$pass){
   
   $db = Connection::getInstance();
-  $req= $db->prepare("SELECT `f_name`,`l_name`,`id_station` FROM `agent` WHERE `cin`=? AND `pass`=? ");
+  $req= $db->prepare("SELECT `cin`,`f_name`,`l_name`,`station`,`line` FROM `agent` WHERE `cin`=? AND `pass`=? ");
   $req->execute(array($cin,$pass));
   $rep = $req->fetch(PDO::FETCH_ASSOC);
   return $rep;
 }
 
-function addAgent($cin,$l_name,$f_name,$id_station,$pass){
+function addAgent($cin,$l_name,$f_name,$line,$station,$pass){
 
   $db = Connection::getInstance();
 
-  $request = $db->prepare('INSERT INTO agent(cin,l_name,f_name,id_station,pass) VALUES (?,?,?,?,?)');
-  $response = $request->execute(array($cin,$l_name,$f_name,$id_station,$pass));
+  $request = $db->prepare('INSERT INTO agent(cin,l_name,f_name,station,`line`,pass) VALUES (?,?,?,?,?,?)');
+  $response = $request->execute(array($cin,$l_name,$f_name,$station,$line,$pass));
 
   if(!$response){
     die(print_r('Error : '.$db->errorInfo()));
@@ -41,12 +41,12 @@ function deleteAgent($cin){
 
 }
 
-function changeAgentLocation($cin,$id_station){
+function changeAgentLocation($cin,$line,$station){
   
   $db = Connection::getInstance();
 
-  $request = $db->prepare('UPDATE agent SET id_station = ? WHERE cin = ?');
-  $response = $request->execute(array($id_station,$cin));
+  $request = $db->prepare('UPDATE agent SET station = ? ,`line`=? WHERE cin = ?');
+  $response = $request->execute(array($station,$line,$cin));
 
   if(!$response){
     die('Error : ').$db->errorInfo();
