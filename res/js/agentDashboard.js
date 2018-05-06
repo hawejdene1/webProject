f056b89dd561f8596a0b97e113b1ad48fee18e4f
hawejdene <hawejdene@gmail.com>
2018-05-06 21:49:26 +0100

    fix some merge problem

--- res/js/agentDashboard.js
+++ res/js/agentDashboard.js
@@ -9,50 +9,34 @@ exitPanel = document.getElementById("exitPanel");
 
 exitBtn.addEventListener('click', function(event) {
 
-	event.preventDefault();
-
-	
-	this.classList.add("active");
-	entryBtn.classList.remove("active");
-	
+    event.preventDefault();
 
 
-	exitPanel.classList.remove("hidden");
-	entryPanel.classList.add("hidden");
-
-<<<<<<< HEAD
-	document.getElementById("checkTicket").focus();
-=======
-	entryPanel.classList.add("hidden");
-	exitPanel.classList.remove("hidden");
+    this.classList.add("active");
+    entryBtn.classList.remove("active");
 
 
->>>>>>> 455377a5f377a33c5b236bf67b3308f554107842
+
+    exitPanel.classList.remove("hidden");
+    entryPanel.classList.add("hidden");
+
+    document.getElementById("checkTicket").focus();
 
 });
 
 
 entryBtn.addEventListener('click', function() {
 
-	event.preventDefault();
+    event.preventDefault();
 
-	console.log("clicked Entry");
-	this.classList.add("active");
-	exitBtn.classList.remove("active");
+    console.log("clicked Entry");
+    this.classList.add("active");
+    exitBtn.classList.remove("active");
 
 
-	entryPanel.classList.remove("hidden");
-	exitPanel.classList.add("hidden"); 
-<<<<<<< HEAD
-	document.getElementById("getTicket").focus();
-=======
+    entryPanel.classList.remove("hidden");
+    exitPanel.classList.add("hidden");
+    document.getElementById("getTicket").focus();
 
->>>>>>> 455377a5f377a33c5b236bf67b3308f554107842
-	
 
 });
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 455377a5f377a33c5b236bf67b3308f554107842
